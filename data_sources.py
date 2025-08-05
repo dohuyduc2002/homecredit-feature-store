ebdb0fc4a5197c4c808ca5c70a67474397cb7b68
Do Huy Duc <dohuyduc.work@gmail.com>
2025-08-05 15:49:33 +0700

    change minio endpoint

--- data_sources.py
+++ data_sources.py
@@ -3,7 +3,7 @@ from feast import (
     FileSource,
     KafkaSource
 )
-from feast.types import Int64, Float32
+
 from feast.infra.offline_stores.contrib.spark_offline_store.spark_source import SparkSource
 from feast.data_format import StreamFormat, AvroFormat
 
@@ -20,22 +20,29 @@ minio_client = Minio(
     secure=False,
 )
 
-application_source = SparkSource(
-    name=source_name,
-    path=minio_path,
-    file_format="csv",
-    timestamp_field="updated",
-    created_timestamp_column="created",
-    description="user application credit source",
-    owner="dohuyduc.work@gmail.com",
-)
 
-# A push source is useful if you have upstream systems that transform features (e.g. stream processing jobs)
-credit_card_balance_push_source = KafkaSource(
-    name="credit_card_balance_kafka_source",
-    timestamp_field="event_timestamp",
-    message_format=JsonFormat(),  # Or your format, e.g., AvroFormat(), ProtobufFormat()
-    bootstrap_servers="kafka:9092",   # Update this to your broker
-    topic="credit-card-balance-topic",
-    # Optionally add batch_source, description, etc.
-)
+def create_spark_source(spark_src_name, minio_path, file_format, timestamp_field, created_timestamp_column):
+    application_source = SparkSource(
+        name=spark_src_name,
+        path=minio_path,
+        file_format=file_format,
+        timestamp_field=timestamp_field,
+        created_timestamp_column=created_timestamp_column,
+        description="user spark source",
+        owner="dohuyduc.work@gmail.com",
+    )
+    return application_source
+
+def create_kafka_source(kafka_src_name, topic, bootstrap_servers, message_format, timestamp_field):
+    # A push source is useful if you have upstream systems that transform features (e.g. stream processing jobs)
+    credit_card_balance_push_source = KafkaSource(
+        name=kafka_src_name,
+        timestamp_field=timestamp_field,
+        message_format=message_format,
+        bootstrap_servers=bootstrap_servers,
+        topic=topic,
+        description="user kafka source",
+        owner="dohuyduc.work@gmail.com",
+    )
+    return credit_card_balance_push_source
+
