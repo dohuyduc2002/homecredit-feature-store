5c6f4f8d98e06822597d7f09239d844be56f5a41
Do Huy Duc <dohuyduc.work@gmail.com>
2025-08-20 23:45:24 +0700

    change entities value to int64 from str

--- feature_repo/entities.py
+++ feature_repo/entities.py
@@ -4,10 +4,10 @@ project = Project(name="homecredit")
 
 # Spark entities
 application_entity = Entity(
-    name="application", join_keys=["sk_id_curr"], value_type=ValueType.STRING
+    name="application", join_keys=["sk_id_curr"], value_type=ValueType.INT64
 )
 
 # Kafka entities
 bureau_balance_entity = Entity(
-    name="bureau-balance", join_keys=["sk_id_curr"], value_type=ValueType.STRING
+    name="bureau-balance", join_keys=["sk_id_curr"], value_type=ValueType.INT64
 )
